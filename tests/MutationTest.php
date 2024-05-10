365cd89fde52360e8770b344301a2350d77aada2
Maksym Mykhailenko <mykhailenko.maks@gmail.com>
2024-05-10 19:21:50 +0200

    Update unit tests

--- tests/MutationTest.php
+++ tests/MutationTest.php
@@ -8,36 +8,36 @@ class MutationTest extends TestCase
 {
     public function testMutation()
     {
-        $string = 'mutation{testing(id:5,test:{test2:5}){test3{test4}}}';
+        $string = 'mutation{testing(id:5,nullarg:null,test:{test2:5}){test3{test4}}}';
 
         $mutation = new Mutation('testing');
         $mutation
             ->addSelect(['test3' => ['test4']])
-            ->addArguments(['id' => 5, 'test' => ['test2' => 5]]);
+            ->addArguments(['id' => 5, 'nullarg' => null, 'test' => ['test2' => 5]]);
 
         $this->assertSame($string, $mutation->getPreparedQuery());
     }
 
     public function testMutation2()
     {
-        $string = 'mutation{testing(id:{user_id:5,user_type:"account"},test:{test2:5}){test3{test4}}}';
+        $string = 'mutation{testing(id:{user_id:5,nullarg:null,user_type:"account"},test:{test2:5}){test3{test4}}}';
 
         $mutation = new Mutation('testing');
         $mutation
             ->addSelect(['test3' => ['test4']])
-            ->addArguments(['id' => ['user_id' => 5, 'user_type' => 'account'], 'test' => ['test2' => 5]]);
+            ->addArguments(['id' => ['user_id' => 5, 'nullarg' => null, 'user_type' => 'account'], 'test' => ['test2' => 5]]);
 
         $this->assertSame($string, $mutation->getPreparedQuery());
     }
 
     public function testMutation3()
     {
-        $string = 'mutation{testing(id:{user_id:"5",user_type:ACCOUNT},test:{test2:5}){test3{test4}}}';
+        $string = 'mutation{testing(id:{user_id:"5",nullarg:null,user_type:ACCOUNT},test:{test2:5}){test3{test4}}}';
 
         $mutation = new Mutation('testing');
         $mutation
             ->addSelect(['test3' => ['test4']])
-            ->addArguments(['id' => ['user_id' => '5', 'user_type' => new Enum('ACCOUNT')], 'test' => ['test2' => 5]]);
+            ->addArguments(['id' => ['user_id' => '5', 'nullarg' => null, 'user_type' => new Enum('ACCOUNT')], 'test' => ['test2' => 5]]);
 
         $this->assertSame($string, $mutation->getPreparedQuery());
     }
--- tests/QueryTest.php
+++ tests/QueryTest.php
@@ -8,24 +8,24 @@ class QueryTest extends TestCase
 {
     public function testQuery()
     {
-        $string = 'query{testing(id:5,test:{test2:5}){test3{test4}}}';
+        $string = 'query{testing(id:5,nullarg:null,test:{test2:5}){test3{test4}}}';
 
         $query = new Query('testing');
         $query
             ->addSelect(['test3' => ['test4']])
-            ->addArguments(['id' => 5, 'test' => ['test2' => 5]]);
+            ->addArguments(['id' => 5, 'nullarg' => null, 'test' => ['test2' => 5]]);
 
         $this->assertSame($string, $query->getPreparedQuery());
     }
 
     public function testQuery2()
     {
-        $string = 'query{testing(id:"5",user_type:ACCOUNT,test:{test2:5}){test3{test4}}}';
+        $string = 'query{testing(id:"5",nullarg:null,user_type:ACCOUNT,test:{test2:5}){test3{test4}}}';
 
         $query = new Query('testing');
         $query
             ->addSelect(['test3' => ['test4']])
-            ->addArguments(['id' => '5', 'user_type' => new Enum('ACCOUNT'), 'test' => ['test2' => 5]]);
+            ->addArguments(['id' => '5', 'nullarg' => null, 'user_type' => new Enum('ACCOUNT'), 'test' => ['test2' => 5]]);
 
         $this->assertSame($string, $query->getPreparedQuery());
     }
